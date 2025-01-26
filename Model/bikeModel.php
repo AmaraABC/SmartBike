cb1d77ed9d50604696bd53a86b3fd7e1a3e41694
AmaraABC <amara-noah.gakou@efrei.net>
2025-01-26 14:54:27 +0100

    New commit : Correction of the function named 'getLastAvailableBike'

--- Model/bikeModel.php
+++ Model/bikeModel.php
@@ -15,7 +15,7 @@ class bikeModel
     // Requête pour récupérer le dernier vélo publié
     public function getLastReleasedBike()
     {
-        return $this->bike->query("SELECT * from bikes order by id_velo desc limit 1")->fetchAll(PDO::FETCH_ASSOC);
+        return $this->bike->query("SELECT * from bikes order by datepublication desc limit 1")->fetchAll(PDO::FETCH_ASSOC);
     }
 
     // Requête pour afficher tous les vélos présents dans la base de données 'SmartBike' à partir du plus récent
