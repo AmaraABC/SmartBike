65d6f7e128245362dd1b2243118ec0b73c854a72
AmaraABC <amara-noah.gakou@efrei.net>
2025-01-26 16:58:43 +0100

    New commit : Enhanced version of the page dedicated to display SmartBike's bikes

---
+++ View/bikes.php
@@ -0,0 +1,21 @@
+<h1 class="smartbike-welcome">Liste de nos vélos</h1>
+
+<!-- Afficher tous les vélos disponibles du catalogue 'SmartBike' -->
+<div class="smartbike-all-bikes">
+    <?php foreach ($bikes as $bikes) { ?>
+        <section class="smartbike-bike">
+            <article class="smartbike-bike-img">
+                <img src="<?php echo $bikes['photo']; ?>" alt="<?php $bikes['contenu']; ?>" height="241.2890625" width="362.5">
+            </article>
+            <article class="smartbike-bike-text">
+                <h3>
+                    <?php echo $bikes['nom']; ?>
+                </h3>
+                <p>
+                    <?php echo $bikes['prix']; ?> €
+                </p>
+            </article>
+        </section>
+        <hr class="smartbike-delimiter">
+    <?php } ?>
+</div>
