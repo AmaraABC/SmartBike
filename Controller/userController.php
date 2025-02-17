0838bf8d3ffd74ed9ea55e619826026c15dc6870
AmaraABC <amara-noah.gakou@efrei.net>
2025-02-17 23:15:12 +0100

    New commit: Addition of a controller and a model for the contact page

---
+++ Controller/userController.php
@@ -0,0 +1,16 @@
+<?php
+include('../Model/userModel.php');
+
+// Définition d'une classe réservée au traitement des informations des utilisateurs
+class userModel {
+    private $model;
+
+    // Récupération des informations issues de la classe 'userModel'
+    public function __construct() {
+        $this->model = new userModel;
+    }
+
+    public function contacts() {
+
+    }
+}
---
+++ Model/userModel.php
@@ -0,0 +1,11 @@
+<?php
+include('smartbike.php');
+
+// Définition d'une classe réservée aux utilisateurs
+class userModel {
+    private $user;
+
+    public function __construct() {
+        $this->user = smartbike::connexion();
+    }
+}
